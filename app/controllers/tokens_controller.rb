a3f9025d32e7d55c9bc7688e9d1c26edf22a676e
zakrapovic <o.naret@gmail.com>
2015-04-16 01:39:52 +0200

    cleaned up toekn

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,5 +1,4 @@
 class TokensController < ApplicationController
-  before_action :set_token, only: [:show, :edit, :update, :destroy]
 
   # GET /tokens
   # GET /tokens.json
@@ -8,18 +7,9 @@ class TokensController < ApplicationController
     @last_token_status = Token.all.last.status
   end
 
-  # GET /tokens/1
-  # GET /tokens/1.json
-  def show
-  end
-
   # GET /tokens/new
   def new
     @token = Token.new
-  end
-
-  # GET /tokens/1/edit
-  def edit
   end
 
   # POST /tokens
@@ -29,37 +19,12 @@ class TokensController < ApplicationController
     @token.user = User.all.first
     respond_to do |format|
       if @token.save
-        format.html { redirect_to @token, notice: 'Token was successfully created.' }
-        format.json { render :show, status: :created, location: @token }
+        format.html { redirect_to tokens_path, notice: 'Token was successfully created.' }
+        format.json { render :show, status: :created, location: tokens_path }
       else
         format.html { render :new }
         format.json { render json: @token.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /tokens/1
-  # PATCH/PUT /tokens/1.json
-  def update
-    respond_to do |format|
-      @token.update(user: User.all.first)
-      if @token.update(token_params)
-        format.html { redirect_to @token, notice: 'Token was successfully updated.' }
-        format.json { render :show, status: :ok, location: @token }
-      else
-        format.html { render :edit }
-        format.json { render json: @token.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /tokens/1
-  # DELETE /tokens/1.json
-  def destroy
-    @token.destroy
-    respond_to do |format|
-      format.html { redirect_to tokens_url, notice: 'Token was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
@@ -71,10 +36,6 @@ class TokensController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_token
-      @token = Token.find(params[:id])
-    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def token_params
